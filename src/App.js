004a234c21afc0e3a52203ecedd3b802e6727769
RamkishoreBandla <bandlaramkishore090@gmail.com>
2022-09-08 03:26:45 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Navigationbar from './navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navigationbar />
+   <br></br>
+    
+    </>
   );
 }
 
