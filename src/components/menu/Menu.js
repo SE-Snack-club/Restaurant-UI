b764767c4807aea05bc1abdd476c75fef1ad4468
RamkishoreBandla <bandlaramkishore090@gmail.com>
2022-09-08 05:33:09 +0530

    added display items

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,87 @@
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './Menu.css';
+import reactlogo from '../../logo.svg';
 
-const Menu=()=>{
-    return(<>
-        <h1>Menu</h1>
-        </>)
+
+const Menu = () => {
+    return (<>
+        <Container className='mt-5'>
+            <Row className='menu-center-text'>
+                <Col>
+                    Today's Menu
+                </Col>
+            </Row>
+            <Row className='menu-center-text mt-3'>
+                <Col>
+                    some thing
+                </Col>
+                <Col> Nothing</Col>
+            </Row>
+
+        </Container>
+
+        <Container className='mt-5'>
+            <Row>
+                <Col>
+                    <Card border="info" >
+                        <Card.Img  height={200} variant="top" src={reactlogo} />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card border="info" >
+                        <Card.Img  height={200} variant="top" src={reactlogo} />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card border="info" >
+                        <Card.Img  height={200} variant="top" src={reactlogo}  />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card border="info" >
+                        <Card.Img  height={200} variant="top" src={reactlogo}  />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    </>)
 }
 
 export default Menu;
