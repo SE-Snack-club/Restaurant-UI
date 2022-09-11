1f13f441ce7f61078b1187fbc748cac26f25e8c7
Bharath1006 <bharath.p1600@gmail.com>
2022-09-10 20:05:27 -0400

    first_commit

---
+++ src/components/foodAllergenInfo/FoodAllergenInfo.js
@@ -0,0 +1,7 @@
+const FoodAllergenInfo=()=>{
+    return(<>
+        <h1>FoodAllergenInfo</h1>
+        </>)
+}
+
+export default FoodAllergenInfo;
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -6,7 +6,7 @@ import Home from '../components/Home/Home';
 import Login from '../components/login/Login';
 import Menu from '../components/menu/Menu';
 import Register from '../components/register/Register';
-
+import FoodAllergenInfo from '../components/foodAllergenInfo/FoodAllergenInfo';
 
 
 const Navigationbar = () => {
@@ -18,15 +18,16 @@ const Navigationbar = () => {
           <Nav className="me-auto">
 
 
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-
+            <Nav.Link as={Link} to="/home">
+              Home</Nav.Link>
             <Nav.Link as={Link} to="/menu">
               Items Menu</Nav.Link>
             <Nav.Link as={Link} to="/login">
               Login</Nav.Link>
             <Nav.Link as={Link} to="/register">
               Sign up</Nav.Link>
-
+            <Nav.Link as={Link} to="/foodAllergenInfo">
+              FoodAllergenInfo</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -36,6 +37,7 @@ const Navigationbar = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/foodAllergenInfo" element={<FoodAllergenInfo />} />
       </ Routes>
     </>
   )
