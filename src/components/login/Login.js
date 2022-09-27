00b3861a6818b84ebd96f09448a577bac075edd9
jotheeswarraghava <jeeda@albany.edu>
2022-09-27 01:50:45 -0400

    modified login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -77,7 +77,7 @@ const Login=()=>{
         <Form.Group as={Col} controlId="validationCustom03" lg={{span:4,offset:4}}>
           <Form.Label>Password</Form.Label>
           <Form.Control 
-          type="text" 
+          type="password" 
           value={password} 
           onChange={(e) => { setPassword(e.target.value) }}
           required />
