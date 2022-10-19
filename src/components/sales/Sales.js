bf7a591abad44aae56247944a1ffa5901649f71e
naveenperam <nperam@albany.edu>
2022-10-19 01:44:05 -0400

    final

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -1,27 +1,14 @@
-
 import React from "react";
 import { MDBContainer } from "mdbreact";
 import { Bar } from "react-chartjs-2";
 
 
-const Sales=()=>{
-    return(
-    <div>
-
-    <MDBCol lg='7'>
-      <MDBChart
-        type='line'
-        data={{
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-          datasets: [
-            {
-              label: 'Traffic',
-              data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
-            },
-          ],
-        }}
-      />
-    </MDBCol>
- </div>
-    )}
+const Sales = () => {
+    
+    return (
+        <div>
+hi
+        </div>
+    )
+}
 export default Sales;
